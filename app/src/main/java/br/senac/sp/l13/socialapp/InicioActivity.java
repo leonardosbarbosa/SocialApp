af5928acae6543e0719302c3b800e2e6f805781f
leonardo.sbarbosa4 <leonardo.sbarbosa2801@gmail.com>
2018-05-29 17:04:25 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/socialapp/InicioActivity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.socialapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_layout);
+    }
+}
